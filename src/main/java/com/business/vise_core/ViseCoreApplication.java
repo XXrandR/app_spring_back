a284df2921e96c4cda6d1d63be01cdfbaf7c780a
Jhosua Dany <jhosuacontjhosua@gmail.com>
2022-11-08 02:26:45 -0500

    first commit

---
+++ src/main/java/com/business/vise_core/ViseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.business.vise_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViseCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/vise_core/ViseCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.vise_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViseCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
